f181ce853701122de6bc367f41c015fbcc5c3c4d
navya2024 <nkandregula170@rku.ac.in>
2022-09-20 15:41:11 +0530

    Dep

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace University_Management_System
 {
-    partial class Form1
+    partial class Home
     {
         /// <summary>
         /// Required designer variable.
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
             this.label15 = new System.Windows.Forms.Label();
             this.pictureBox15 = new System.Windows.Forms.PictureBox();
             this.pictureBox14 = new System.Windows.Forms.PictureBox();
