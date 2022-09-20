9bdf040093c9dca845fafaf6bf414ce93961096a
navya2024 <nkandregula170@rku.ac.in>
2022-09-20 16:18:30 +0530

    st

--- Departments.Designer.cs
+++ Departments.Designer.cs
@@ -66,6 +66,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel4 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
@@ -461,12 +462,21 @@
             this.dataGridView1.Size = new System.Drawing.Size(762, 220);
             this.dataGridView1.TabIndex = 87;
             // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DimGray;
+            this.panel4.Location = new System.Drawing.Point(56, 276);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(100, 5);
+            this.panel4.TabIndex = 88;
+            // 
             // Departments
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(946, 583);
+            this.Controls.Add(this.panel4);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.button2);
@@ -564,5 +574,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel4;
     }
 }
