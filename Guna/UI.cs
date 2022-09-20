ff8aa9b4ab42c3731db0beddee0f0624c00733e3
navya2024 <nkandregula170@rku.ac.in>
2022-09-20 15:20:09 +0530

    Departments

---
+++ Guna/UI.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Guna
+{
+    internal class UI
+    {
+        internal class WinForms
+        {
+            internal class GunaElipse
+            {
+                private IContainer components;
+
+                public GunaElipse(IContainer components)
+                {
+                    this.components = components;
+                }
+
+                public int Radius { get; internal set; }
+                public Panel TargetControl { get; internal set; }
+
+                internal class UI
+                {
+                    internal class WinForms
+                    {
+                        internal class GunaElipse : Guna.UI.WinForms.GunaElipse
+                        {
+                            private IContainer components;
+
+                            public GunaElipse(IContainer components)
+                            {
+                                this.components = components;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
