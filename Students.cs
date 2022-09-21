a79d1ba5a2060ad9458803c12f306bb750a934a5
Goral <gparmar754@rku.ac.in>
2022-09-21 10:47:12 +0530

    stuchanges

--- Students.cs
+++ Students.cs
@@ -16,5 +16,15 @@ namespace University_Management_System
         {
             InitializeComponent();
         }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label12_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
