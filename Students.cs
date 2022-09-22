3788c3af164c80f7d1105a0303978a79b2138df0
Goral <gparmar754@rku.ac.in>
2022-09-22 09:08:27 +0530

    stutable

--- Students.cs
+++ Students.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace University_Management_System
 {
@@ -15,7 +16,34 @@ namespace University_Management_System
         public Students()
         {
             InitializeComponent();
+            ShowStudent();
         }
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Goral\Source\Repos\vishakhajp\University_Management_System\AppData\RKUniverity.mdf;Integrated Security=True");
+        private void ShowStudent()
+        {
+            con.Open();
+            string query = "select * from Student";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            sda.Fill(ds);
+            //Student.DataSource = ds.Tables[0];
+            con.Close();
+        }
+
+        private void Reset()
+        {
+            //DepName.Text = "";
+            //stdTb.Text = "";
+            //stdGencb.SelectedIndex = - 1;
+            //PhoneTb.Text = "";
+            //StdAddTb.Text = "";
+            //depIdCb.selectedIndex = -1;
+
+        }
+
+
 
         private void Button2_Click(object sender, EventArgs e)
         {
@@ -23,6 +51,11 @@ namespace University_Management_System
         }
 
         private void Label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_Click(object sender, EventArgs e)
         {
 
         }
